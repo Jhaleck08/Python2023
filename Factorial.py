eae08840a4bfc355d6877e6764225848f5c71b45
Jhaleck <129187877+Jhaleck08users.noreply.github.com>
2023-04-03 16:27:51 -0400

    Mi primer commit

--- Factorial.py
+++ Factorial.py
@@ -2,6 +2,6 @@ def factorial(num):
     print("Valor inicial ->",num)
     if num > 1:
         num = num * factorial(num - 1)
-        print("Valor final ->",num)
-        return num
+    print("Valor final ->",num)
+    return num
 print(factorial(5))
